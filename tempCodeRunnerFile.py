a798cadcfbc791886aad43ba21eb3e0bbe0b7b52
Sadique Iqbal <sadiqueiqbal28@gmail.com>
2025-09-06 02:21:45 +0530

    removed temp code runner file

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-with open("system-monitoring.pdf",'w') as f:
-#     f.write(f"CPU usage: {cpu_usage}%\n")
-#     f.write(f"Memory usage: {mem_usage}%\n")
-#     f.write(f"Disk usage: {disk_usage}%")
